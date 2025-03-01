67d49950db531dd8c0f7eaff6d470efcc13101e1
GionaCantarutti <cantarutti.giona@spes.uniud.it>
2025-03-01 19:17:25 +0100

    Switched to JSON5

--- src/mod.ts
+++ src/mod.ts
@@ -1,28 +1,36 @@
+import fs from "fs";
+import JSON5 from "json5";
 import { DependencyContainer } from "tsyringe";
-
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { Context } from "./contex";
 import { algorithmicallyRebalance } from "./algoRebalancing/core";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
+import { getModFolder } from "./utils";
 
-class Mod implements IPostDBLoadMod
+class Mod implements IPostDBLoadMod, IPreSptLoadMod
 {
     // config and context
-    private config = require("../config/config.json");
+    //private config = require("../config/config.json");
     private context: Context;
+    
+    preSptLoad(container: DependencyContainer): void
+    {
+        this.context = new Context();
+        this.context.logger = container.resolve<ILogger>("WinstonLogger");
+
+        //Read config
+        const fileContent = fs.readFileSync(getModFolder() + "/config/config.json5", "utf-8");
+        this.context.config = JSON5.parse(fileContent);
+    }
 
     public postDBLoad(container: DependencyContainer): void
     {
-        this.context = new Context(
-            this.config,
-            container.resolve<DatabaseServer>("DatabaseServer"),
-            null,
-            container.resolve<ILogger>("WinstonLogger")
-        );
+        this.context.database = container.resolve<DatabaseServer>("DatabaseServer");
         this.context.tables = this.context.database.getTables();
 
-        if (this.config.algorithmicalRebalancing.enableAlgorithmicalRebalancing) algorithmicallyRebalance(container, this.context);
+        if (this.context.config.algorithmicalRebalancing.enable) algorithmicallyRebalance(container, this.context);
         
     }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,15 @@
 import { ITrader } from "@spt/models/eft/common/tables/ITrader";
 
+//Sets the loyalty level of the given sale item on the given trader
+//If capToMax is false the loyalty level will be allowed to go beyond 4
 export function setLoyalty(itemID: string, loyalty: number, trader: ITrader, capToMax: boolean): void
 {
+    if (loyalty == null) loyalty = 0;
     const maxLevel = capToMax ? 4 : 999 //Set max level to a dummy value if loyalty > 4 is to be hidden from all trader levels in config
     trader.assort.loyal_level_items[itemID] = Math.max(1, Math.min(maxLevel, Math.floor(loyalty))); //Floor and clamp between 1 and the max level
+}
+
+export function getModFolder(): string {
+    return "./user/mods/Gekos_BetterProgression";
+}
 }
